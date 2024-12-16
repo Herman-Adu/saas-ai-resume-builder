5d64739bd172230419cf839272b94a2a067f6b73
Herman Adu <herman.adu@hotmail.com>
2024-12-16 17:56:04 +0000

    Resume editor, React Hook Form, FormField, useFieldArray, Zod refine, server actions, auto-save hook

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "4mb",
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "b7pf7ojaqrlw1vpi.public.blob.vercel-storage.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ src/app/(main)/editor/actions.ts
@@ -0,0 +1,110 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { resumeSchema, ResumeValues } from "@/lib/validation";
+import { auth } from "@clerk/nextjs/server";
+import { del, put } from "@vercel/blob";
+import path from "path";
+
+export async function saveResume(values: ResumeValues) {
+  const { id } = values;
+
+  console.log("received values", values);
+
+  // validate the Resume values
+  const { photo, workExperiences, educations, ...resumeValues } =
+    resumeSchema.parse(values);
+
+  // get user
+  const { userId } = await auth();
+
+  // check user
+  if (!userId) {
+    throw new Error("User not authenticated");
+  }
+
+  // TODO: Check resume count for non-premium users
+
+  // get resumee from daataabase
+  const existingResume = id
+    ? await prisma.resume.findUnique({ where: { id, userId } })
+    : null;
+
+  // check we have an id and no existing resume - then something is wrong
+  if (id && !existingResume) {
+    throw new Error("Resume not found");
+  }
+
+  // upload photo to vercel blob sttorage
+  let newPhotoUrl: string | undefined | null = undefined;
+
+  if (photo instanceof File) {
+    // upload file to blob storage
+
+    // check for existing photo - and delete if one exsist
+    if (existingResume?.photoUrl) {
+      await del(existingResume.photoUrl);
+    }
+
+    // upload file to blob
+    const blob = await put(`resume_photos/${path.extname(photo.name)}`, photo, {
+      access: "public",
+    });
+
+    newPhotoUrl = blob.url;
+  } else if (photo === null) {
+    if (existingResume?.photoUrl) {
+      await del(existingResume.photoUrl);
+    }
+    newPhotoUrl = null;
+  }
+
+  if (id) {
+    return prisma.resume.update({
+      where: { id },
+      data: {
+        ...resumeValues,
+        photoUrl: newPhotoUrl,
+        workExperiences: {
+          deleteMany: {},
+          create: workExperiences?.map((exp) => ({
+            ...exp,
+            startDate: exp.startDate ? new Date(exp.startDate) : undefined,
+            endDate: exp.endDate ? new Date(exp.endDate) : undefined,
+          })),
+        },
+        educations: {
+          deleteMany: {},
+          create: educations?.map((edu) => ({
+            ...edu,
+            startDate: edu.startDate ? new Date(edu.startDate) : undefined,
+            endDate: edu.endDate ? new Date(edu.endDate) : undefined,
+          })),
+        },
+        updatedAt: new Date(),
+      },
+    });
+  } else {
+    return prisma.resume.create({
+      data: {
+        ...resumeValues,
+        userId,
+        photoUrl: newPhotoUrl,
+        workExperiences: {
+          create: workExperiences?.map((exp) => ({
+            ...exp,
+            startDate: exp.startDate ? new Date(exp.startDate) : undefined,
+            endDate: exp.endDate ? new Date(exp.endDate) : undefined,
+          })),
+        },
+        educations: {
+          create: educations?.map((edu) => ({
+            ...edu,
+            startDate: edu.startDate ? new Date(edu.startDate) : undefined,
+            endDate: edu.endDate ? new Date(edu.endDate) : undefined,
+          })),
+        },
+      },
+    });
+  }
+}
