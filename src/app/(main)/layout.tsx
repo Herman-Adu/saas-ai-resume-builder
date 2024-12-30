705281141c854b40b74ec77bb47fd7bffd124f92
Herman Adu <herman.adu@hotmail.com>
2024-12-30 13:07:26 +0000

    added subscription table to prisma, implemented saving and fetching subscriptions utilising react cache and context provider

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,12 +1,33 @@
 import PremiumModal from "@/components/premium/PremiumModal";
+import { getUserSubscriptionLevel } from "@/lib/subscription";
+import { auth } from "@clerk/nextjs/server";
 import Navbar from "./Navbar";
+import SubscriptionLevelProvider from "./SubscriptionLevelProvider";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  //get the user
+  const { userId } = await auth();
+
+  // check we got a user
+  if (!userId) {
+    return null;
+  }
+
+  // get user subscsription level
+  const userSubscriptionLevel = await getUserSubscriptionLevel(userId);
+
+  // wrap main layout in subscription provider to be availaable to all child client component as its where we can access context
   return (
-    <div className="flex min-h-screen flex-col">
-      <Navbar />
-      {children}
-      <PremiumModal />
-    </div>
+    <SubscriptionLevelProvider userSubscriptionLevel={userSubscriptionLevel}>
+      <div className="flex min-h-screen flex-col">
+        <Navbar />
+        {children}
+        <PremiumModal />
+      </div>
+    </SubscriptionLevelProvider>
   );
 }
