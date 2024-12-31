060053d70812ab13513f03f993418b88edf9b83e
Herman Adu <herman.adu@hotmail.com>
2024-12-31 18:26:22 +0000

    Implemented stripe customer portal allowing a user to cancel, renew and manage their subscription

---
+++ src/app/(main)/billing/actions.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { env } from "@/env";
+import stripe from "@/lib/stripe";
+import { currentUser } from "@clerk/nextjs/server";
+
+export async function createCustomerPortalSession() {
+  // get the current user object
+  const user = await currentUser();
+
+  // check the user is defined
+  if (!user) {
+    throw new Error("Unauthorized");
+  }
+
+  // get the customerId stored in the private metadata in clerk
+  const stripeCustomerId = user.privateMetadata.stripeCustomerId as
+    | string
+    | undefined;
+
+  // check we got the stripe customerId from clerk private metadata
+  if (!stripeCustomerId) {
+    throw new Error("Stripe customer ID not found");
+  }
+
+  // create a CustomerPortalSession and redirect back to the billing page
+  const session = await stripe.billingPortal.sessions.create({
+    customer: stripeCustomerId,
+    return_url: `${env.NEXT_PUBLIC_BASE_URL}/billing`,
+  });
+
+  // check we got a session url, otherwise throww error
+  if (!session.url) {
+    throw new Error("Failed to create customer portal session");
+  }
+
+  // return session url
+  return session.url;
+}
--- src/app/(main)/billing/page.tsx
+++ src/app/(main)/billing/page.tsx
@@ -1,3 +1,60 @@
-export default function Page() {
-  return <main>Billing</main>;
+import prisma from "@/lib/prisma";
+import stripe from "@/lib/stripe";
+import { auth } from "@clerk/nextjs/server";
+import { formatDate } from "date-fns";
+import { Metadata } from "next";
+import Stripe from "stripe";
+import GetSubscriptionButton from "./GetSubscriptionButton";
+import ManageSubscriptionButton from "./ManageSubscriptionButton";
+
+export const metadata: Metadata = {
+  title: "Billing",
+};
+
+// here I want to fetch the subscription for this user to show some information on the page
+export default async function Page() {
+  // get the clerk userId
+  const { userId } = await auth();
+
+  // check we have the user
+  if (!userId) {
+    return null;
+  }
+
+  // if we have a userId, get the subscription object from the database
+  const subscription = await prisma.userSubscription.findUnique({
+    where: { userId },
+  });
+
+  // get the price info for current subscription object so that I can show the name of the current subscription on the page
+  const priceInfo = subscription
+    ? await stripe.prices.retrieve(subscription.stripePriceId, {
+        expand: ["product"],
+      })
+    : null;
+
+  return (
+    <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
+      <h1 className="text-3xl font-bold">Billing</h1>
+      <p>
+        Your current plan:{" "}
+        <span className="font-bold">
+          {priceInfo ? (priceInfo.product as Stripe.Product).name : "Free"}
+        </span>
+      </p>
+      {subscription ? (
+        <>
+          {subscription.stripeCancelAtPeriodEnd && (
+            <p className="text-destructive">
+              Your subscription will be canceled on{" "}
+              {formatDate(subscription.stripeCurrentPeriodEnd, "MMMM dd, yyyy")}
+            </p>
+          )}
+          <ManageSubscriptionButton />
+        </>
+      ) : (
+        <GetSubscriptionButton />
+      )}
+    </main>
+  );
 }
